5f5081d98a3665e5fc3227461b28326bf3451ba5
TianTran <tian.tran87@gmail.com>
2021-05-23 15:02:46 +1000

    Update Week 2

---
+++ Jupyter-Workspace/Week_2/Day 1/conditionally_yours_part_1-Copy1.py
@@ -0,0 +1,25 @@
+"""
+Conditionally Yours
+
+Pseudocode:
+
+1. Create variables for the following:
+
+    * `original_price`
+    * `current_price`
+    * `increase`
+    * `percent_increase`
+
+2. Derive `increase`:
+    * Increase = Current Price - Original Price
+    
+3. Derive `percent_increase`:
+    * Percent Increase = Increase / Original x 100
+    
+4. Create the logic:
+    * If percent_increase > 20% => Sell 
+        Else HOLD
+    * If percent_increase < -20% => Buy
+        Else HOLD
+
+"""
